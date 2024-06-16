97cc7761c03e10b46d046a3e666fccf2ed0d7bc5
XH2 <546151373@qq.com>
2024-06-16 12:24:47 +0800

    注入

--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -4,12 +4,23 @@ import org.example.dao.BookDao;
 import org.example.dao.impl.BookDaoImpl;
 import org.example.service.BookService;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
+
 public class BookServiceImpl implements BookService {
   private BookDao bookDao;
+  private String name;
+  private int[] ages;
+  private Map<String, String> myMap;
+  private Properties myProperties;
   @Override
   public void save() {
     bookDao.save();
-    System.out.println("BookService save");
+    System.out.println("BookService save" + name);
+    System.out.println("ages" + Arrays.toString(ages));
+    System.out.println("myMap" + myMap.toString());
+    System.out.println("myProperties" + myProperties.toString());
   }
 
   public void setBookDao(BookDao bookDao) {
@@ -22,5 +33,21 @@ public class BookServiceImpl implements BookService {
 
   public void destroy() {
     System.out.println("destroy");
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setAges(int[] ages) {
+    this.ages = ages;
+  }
+
+  public void setMyMap(Map<String, String> myMap) {
+    this.myMap = myMap;
+  }
+
+  public void setMyProperties(Properties myProperties) {
+    this.myProperties = myProperties;
   }
 }
